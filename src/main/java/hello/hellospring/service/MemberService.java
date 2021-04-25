17cbb4d13a33f651ccb551b5a45dcc3ab7c372d3
limi6n <6nlimitless@gmail.com>
2021-04-26 00:41:24 +0900

    회원 관리 예제 - 웹 MVC

---
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,47 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRespository;
+import hello.hellospring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRespository memberRespository;
+
+    @Autowired
+    public MemberService(MemberRespository memberRespository) {
+        this.memberRespository = memberRespository;
+    }
+
+    /**
+     * 회원 가입
+     */
+    public Long join(Member member) {
+        validateDuplicateMember(member); //회원 이름이 같을 경우 중복회원으로 간주(조건)
+        memberRespository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRespository.findByName(member.getName())
+                .ifPresent(m ->{
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers() {
+        return memberRespository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRespository.findById(memberId);
+    }
+}
